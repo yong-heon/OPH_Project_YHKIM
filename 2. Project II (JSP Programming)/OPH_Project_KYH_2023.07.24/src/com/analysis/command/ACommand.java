53bb2a7dbba0e911dd5f7231ce24ad97cfe4761d
yong-heon <131658318+yong-heon@users.noreply.github.com>
2023-07-25 18:02:01 +0900

    Delete 2. Project II (JSP Programming)/OPH_Project_KYH_2023.07.24 directory

--- 2. Project II (JSP Programming)/OPH_Project_KYH_2023.07.24/src/com/analysis/command/ACommand.java
+++
@@ -1,8 +0,0 @@
-package com.analysis.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ACommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
